b08956a02506ed97b6b27549bbc70cdaafc41a88
yoonyoungkim <yoonyoungkim89@gmail.com>
2018-02-01 15:45:10 +0900

    detail css 적용

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -3,8 +3,8 @@ import './Detail.css';
 
 const Detail = () => {
     return (
-        <div className="detail">
-            <div className="summary container">
+        <div className="detail container">
+            <div className="summary">
                 <div className="title">
                     lala-clipping
                 </div>
@@ -14,6 +14,16 @@ const Detail = () => {
                 <div className="tags">
                     <span>#android</span>
                     <span>#nodejs</span>
+                </div>
+            </div>
+
+            <div className="card-item">
+                <div className="img" > 
+                    <img src="https://lh3.googleusercontent.com/raiF0nhQR3ODIFgby-vPcW6XpIaFZhtbFOnsTqFOwpN3CHfYtsd5QXUWDEpDL0puf94=h900-rw"/>
+                </div>
+                <div className="text">
+                    <div className="title">advertisement</div>
+                    <div>advertisement, best presentation, biz, business, corporate, creative, devices, ecommerce, elegant, enterprise, entrepreneur, excel, keynote, marketing, minimal, mockup, modern, multipurpose, Pitch Deck Slides, seo, simple, smart, social media, start up, statistics, stats, swot analysis, tech</div>
                 </div>
             </div>
         </div>
