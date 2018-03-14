72a01f4f556e03edcdffa20d30a671e2cf1b9a82
yoonyoungkim <yoonyoungkim89@gmail.com>
2018-03-14 15:30:05 +0900

    deploy - shell file

--- startup.sh
+++
@@ -1,6 +0,0 @@
-PID=`ps -eaf | grep lala-profile-web | grep -v grep | awk '{print $2}'`
-if [[ "" !=  "$PID" ]]; then
-  echo "killing $PID"
-  kill -9 $PID
-fi
-serve -s /home/ec2-user/targets/lala-profile-web/build
